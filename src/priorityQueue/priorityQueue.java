30c92e6d20c2c3b7dbe279fc46d4c4657ebb924d
Adrianhammer <adrianh@uia.no>
2021-03-26 15:56:03 +0100

    Fremgang på priorityQueue. Klart den ish

--- src/priorityQueue/priorityQueue.java
+++ src/priorityQueue/priorityQueue.java
@@ -2,15 +2,47 @@ package priorityQueue;
 
 
 import java.io.Serializable;
-import java.util.AbstractQueue;
-import java.util.Iterator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class priorityQueue{
 
     public static void main(String[] args) {
 
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("Terje", 5);
+        hm.put("Kari", 7);
+        hm.put("Nils", 4);
+        hm.put("Otto", 8);
+        hm.put("Syvert", 7);
+        hm.put("Lise", 11);
+        hm.put("Notto", 0);
+        hm.put("Odd", 1);
+        hm.put("Even", 2);
+
+        System.out.println("Unordered list: " + hm);
+
+        PriorityQueue<String> pq = new PriorityQueue<>(Comparator.comparingInt(hm::get).reversed());
+        System.out.println("\nPrioritized list from Highest to Lowest: ");
+        pq.add("Terje");
+        pq.add("Otto");
+        pq.add("Even");
+        pq.add("Lise");
+        pq.add("Odd");
+        pq.add("Syvert");
+        pq.add("Kari");
+        pq.add("Nils");
+        pq.add("Notto");
+
+
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
 
     }
-
 }
